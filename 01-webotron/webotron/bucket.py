3d914aeb48acce9d2a6530fbd80f9b73fd7bdbf2
rewilliams <rewilliams@ubuntu.localdomain>
2018-12-10 22:47:12 +0000

    bucket.py update

---
+++ 01-webotron/webotron/bucket.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8-
+"""Code to build static website."""
+
+from pathlib import Path
+import mimetypes
+
+from botocore.exceptions import ClientError
+
+"""Classes for S3 Buckets."""
+
+class BucketManager:
+    """All functions to manage bucket."""
+    
+    def __init__(self, session):
+        """Create a BucketManager object."""
+        self.session = session
+        self.s3 = self.session.resource('s3')
+
+    def all_buckets(self):
+        """Get an iterator for all buckets."""
+        return self.s3.buckets.all()
+
+    def all_objects(self, bucket_name):
+        """Get an iterator for all objects in bucket."""
+        return self.s3.Bucket(bucket_name).objects.all()
+
+    def init_bucket(self, bucket_name):
+        """Create new bucket, or return existing one by name."""
+        s3_bucket = None
+        try:
+            s3_bucket = self.s3.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={
+                    'LocationConstraint': self.session.region_name
+                }
+            )
+        except ClientError as error:
+            if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+                s3_bucket = self.s3.Bucket(bucket_name)
+            else:
+                raise error
+
+        return s3_bucket
+
+    def set_policy(self, bucket):
+        """Set bucket policy to be readable by everyone."""
+        policy = """
+        {
+          "Version":"2012-10-17",
+          "Statement":[{
+          "Sid":"PublicReadGetObject",
+          "Effect":"Allow",
+          "Principal": "*",
+              "Action":["s3:GetObject"],
+              "Resource":["arn:aws:s3:::%s/*"
+              ]
+            }
+          ]
+        }
+        """ % bucket.name
+
+        policy = policy.strip()
+
+        pol = bucket.Policy()
+        pol.put(Policy=policy)
+
+
+    def configure_website(self, bucket):
+        """Configure website for bucket."""
+        bucket.Website().put(WebsiteConfiguration={
+            'ErrorDocument': {
+                'Key': 'string'
+                },
+            'IndexDocument': {
+                'Suffix': 'string'
+                    }
+            })
+
+    @staticmethod
+    def upload_file(bucket, path, key):
+        """Upload path to s3 bucket."""
+        content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+        return bucket.upload_file(
+            path,
+            key,
+            ExtraArgs={
+                'ContentType': content_type
+            })
+
+    def sync_bucket(self, pathname, bucket_name):
+        """Sync contents of path to bucket."""
+        bucket = self.s3.Bucket(bucket_name)
+
+        root = Path(pathname).expanduser().resolve()
+
+        def handle_directory(target):
+            for path in target.iterdir():
+                if path.is_dir():
+                    handle_directory(path)
+                if path.is_file():
+                    self.upload_file(bucket, str(path), str(path.relative_to(root)))
+
+        handle_directory(root)
